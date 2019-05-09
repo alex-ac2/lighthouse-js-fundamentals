de4d5d7244f3a78559de530453d33a3b04e0e72c
Alex Curci <alex.curci@gmail.com.com>
2019-05-09 20:02:24 +0000

    even.js exercise with 'isEven' function completed

--- hello.js
+++ hello.js
@@ -1,5 +1,4 @@
 // Say Hello function 
-
 function sayHello(name) {
     console.log("Hello, " + name);
 }
@@ -7,3 +6,13 @@ function sayHello(name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+
+// Say Hello function w/ return statement
+function returnSayHello(name) {
+      return "Hello, " + name;
+}
+
+var greeting = returnSayHello('John');
+console.log(greeting);
+
